931bdf37dab8bdab64528cb757ce8e539012090e
lilsecret <chuckm248@gmail.com>
2024-03-14 15:13:29 -0600

    set up basic structure in react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Traffic Light App</h1>
+      <div className="container">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
